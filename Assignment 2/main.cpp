c21044ed34877d2dbb103d64028aef51d2d826b3
Simar Singh <simarsingh@Simars-MacBook-Air.local>
2014-11-02 01:47:39 -0500

    Initial Commit

---
+++ Assignment 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 2
+//
+//  Created by Simar Singh on 2014-11-02.
+//  Copyright (c) 2014 Simar Singh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
